cf8230914c87b01c49e80b2d3fcd95301ec66110
Matt Insler <mattinsler@users.noreply.github.com>
2021-01-10 13:26:33 -0800

    improved sorting and tests

--- src/transforms.ts
+++ src/transforms.ts
@@ -1,6 +1,6 @@
 import { traverse } from './traverse';
-import { isNumber, isObjectProperty, isString } from './tools';
 import { ASTArray, ASTNode, ASTObject, Path, Visitor } from './types';
+import { isBoolean, isNumber, isObjectProperty, isString } from './tools';
 
 export type Transform = Visitor;
 export type Predicate<N extends ASTNode> = (path: Path<N>) => boolean;
@@ -9,6 +9,9 @@ const TruePredicate = () => true;
 export function applyTransforms(ast: ASTNode, transforms: Transform[]) {
   transforms.forEach((transform) => traverse(ast, transform));
 }
+
+const ORDER_OF_TYPES: ASTNode['type'][] = ['Null', 'Boolean', 'Number', 'String', 'Array', 'Object'];
+const TYPE_ORDERING = new Map(ORDER_OF_TYPES.map((type, idx) => [type, idx]));
 
 export function sortArrays(predicate: Predicate<ASTArray> = TruePredicate): Transform {
   return {
@@ -22,7 +25,10 @@ export function sortArrays(predicate: Predicate<ASTArray> = TruePredicate): Tran
             if (isNumber(l) && isNumber(r)) {
               return l.value - r.value;
             }
-            return 0;
+            if (isBoolean(l) && isBoolean(r)) {
+              return Number(l.value) - Number(r.value);
+            }
+            return TYPE_ORDERING.get(l.type)! - TYPE_ORDERING.get(r.type)!;
           });
         }
       },
