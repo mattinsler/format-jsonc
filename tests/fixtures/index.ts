c42ec44fd5fc35c3f37e7e630e64b5a4878cbe7c
Matt Insler <mattinsler@users.noreply.github.com>
2021-01-10 12:48:19 -0800

    added test files for ensuring comments are printed in the right places

--- tests/fixtures/index.ts
+++ tests/fixtures/index.ts
@@ -2,6 +2,8 @@ import fs from 'fs';
 import path from 'path';
 
 export const fixtures = {
+  commentsOriginal: fs.readFileSync(path.join(__dirname, 'comments-original.json'), 'utf-8'),
+  commentsFormatted: fs.readFileSync(path.join(__dirname, 'comments-formatted.json'), 'utf-8'),
   original: fs.readFileSync(path.join(__dirname, 'original.json'), 'utf-8'),
   format: fs.readFileSync(path.join(__dirname, 'format.json'), 'utf-8'),
   formatSorted: fs.readFileSync(path.join(__dirname, 'format-sorted.json'), 'utf-8'),
